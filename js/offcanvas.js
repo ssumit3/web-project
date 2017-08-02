0a239a90cbb02a42fb46e1a3dcfbc4db647318c1
ssumit3 <studentsumit_sharma@yahoo.com>
2017-08-02 22:07:42 +0530

    Add files via upload

---
+++ js/offcanvas.js
@@ -0,0 +1,22 @@
+function PrintElem(elem)
+    {
+        Popup($(elem).html());
+    }
+
+    function Popup(data) 
+    {
+        var mywindow = window.open('', 'my div', 'height=400,width=600');
+        mywindow.document.write('<html><head><title>NSC 2016-NITW Form</title>');
+        mywindow.document.write('<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" />');
+        mywindow.document.write('</head><body >');
+        mywindow.document.write(data);
+        mywindow.document.write('</body></html>');
+
+        mywindow.document.close(); // necessary for IE >= 10
+        mywindow.focus(); // necessary for IE >= 10
+
+        mywindow.print();
+        mywindow.close();
+
+        return true;
+    }
